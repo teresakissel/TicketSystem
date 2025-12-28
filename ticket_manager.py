15df33441e688088387131b3a3b6af3678be18cb
teresakissel <teresakissel02@gmail.com>
2025-12-27 19:32:17 -0500

    Implement Banking Support Ticket System

---
+++ ticket_manager.py
@@ -0,0 +1,29 @@
+from utils import load_tickets, save_tickets, create_tickets, display_tickets, update_ticket_status
+def main():
+  tickets = load_tickets()
+
+while True:
+  print("n=== Banking Support Ticket System ===")
+  print("1. Create Support Ticket")
+  Print("2. View All Tickets")
+  print("3. Update Ticket Status")
+  print("4. Exit")
+
+  choice = input("Enter Selection: ")
+
+  if choice == "1":
+    create_ticket(tickets)
+  elif choice == "2":
+    display_tickets(tickets)
+  elif choice == "3":
+    update_ticket_status(tickets)
+  elif choice == "4":
+    save_tickets(tickets)
+    print("Exiting system. Tickets saved.")
+    break
+  else:
+    print("Invalid Selection. Try again.")
+
+if_name_=="_main_":
+  main()
+  
